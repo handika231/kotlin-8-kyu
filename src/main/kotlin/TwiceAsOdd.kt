13364a95a52ef88b9c096a4de93a808b0ab4d0ed
Handika Rafli A <shandika231@gmail.com>
2022-09-05 05:53:28 +0700

    solve twice as odd

---
+++ src/main/kotlin/TwiceAsOdd.kt
@@ -0,0 +1,11 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+
+}
+
+fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int {
+    // your code here
+  val result =  (sonYearsOld * 2 ) - dadYearsOld
+    return result.absoluteValue
+}
