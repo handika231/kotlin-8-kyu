90a41d0d08f36894c669a622c368977dd130f975
Handika Rafli A <shandika231@gmail.com>
2022-09-05 06:04:03 +0700

    solve count odd number

---
+++ src/main/kotlin/CountOddNumber.kt
@@ -0,0 +1,4 @@
+fun oddCount(n: Int): Int {
+    // your code here
+    return (n / 2)
+}
--- src/main/kotlin/TwiceAsOdd.kt
+++ src/main/kotlin/TwiceAsOdd.kt
@@ -9,3 +9,6 @@ fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int {
   val result =  (sonYearsOld * 2 ) - dadYearsOld
     return result.absoluteValue
 }
+
+//BEST SOLUTION
+//fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int = Math.abs(dadYearsOld - (sonYearsOld * 2))
